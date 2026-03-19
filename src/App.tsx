775e0fc1e5750d6fe795ff130a6bea0ccaf2053b
AliouneDIME <aliounedime92@gmail.com>
2026-03-19 05:20:32 +0000

    Corrige bug netlify

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const Certifications = lazy(() => import('./components/Certifications').then(m =
 const Projects       = lazy(() => import('./components/Projects').then(m => ({ default: m.Projects })));
 const Skills         = lazy(() => import('./components/Skills').then(m => ({ default: m.Skills })));
 const ITAdmin        = lazy(() => import('./components/Itadmin').then(m => ({ default: m.ITAdmin })));
-const Blog           = lazy(() => import('./components/blog').then(m => ({ default: m.Blog })));
+const Blog           = lazy(() => import('./components/Blogs').then(m => ({ default: m.Blog })));
 const Contact        = lazy(() => import('./components/Contact').then(m => ({ default: m.Contact })));
 
 /** Minimal placeholder while a lazy section loads */
